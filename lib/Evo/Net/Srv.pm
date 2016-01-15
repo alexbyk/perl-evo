a53a87adb6da6bacd0565a63d420c6ced358f299
alexbyk <alex@alexbyk.com>
2016-01-15 14:45:57 +0200

    add documentation to -Net::Srv

--- lib/Evo/Net/Srv.pm
+++ lib/Evo/Net/Srv.pm
@@ -16,3 +16,25 @@ sub srv_handle_error($self, $conn, $err) : Override {
 }
 
 1;
+
+=head1 SYNOPSYS
+
+  use Evo '-Net::Srv; -Loop *';
+
+  my $srv = Evo::Net::Srv::new();
+  my $listen_socket = $srv->srv_listen(ip => '*', port => 8080);
+
+  $srv->on(
+    srv_accept => sub($srv, $sock) {
+      my ($ip, $port) = $sock->socket_remote;
+      say sprintf("New connection from %s[%s], [%s]", $ip, $port, scalar localtime);
+    }
+  );
+
+  loop_start;
+
+=head1
+
+Mostly for testing purposes. For writing servers, see L<Evo::Net::Srv::Role>
+
+=cut
