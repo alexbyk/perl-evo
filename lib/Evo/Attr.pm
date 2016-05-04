80676384669d81c61c353ed28cb71b92b1901bc8
alexbyk <alex@alexbyk.com>
2016-05-05 01:10:25 +0300

    add Evo::Attr docs

--- lib/Evo/Attr.pm
+++ lib/Evo/Attr.pm
@@ -29,3 +29,60 @@ export_gen attr_handler => sub($provider) {
 
 
 1;
+
+
+=head1 SYNOPSYS
+
+  # Foo.pm
+  package Foo;
+  use Evo '-Attr attr_handler; -Export import';
+
+  attr_handler(
+    sub ($pkg, $code, @attrs) {
+      my @found     = grep {/^Foo/} @attrs;
+      my @remaining = grep { !/^Foo/ } @attrs;
+      say "found in $pkg ($code): " . join ', ', @found if @found;
+      @remaining;
+    }
+  );
+
+
+  # test.pl
+  use Evo 'Foo *';
+  sub foo : Foo {...}
+
+=head1 DESCRIPTION
+
+Provides a nice way to use attributes without limitations.
+
+=head1 USAGE
+
+Package that provides attribute should call L</attr_handler> with a handler as a first argument. That handler should examine attributes, and return unknown for other handlers.
+
+Provider should also import L<Evo::Export/import> to be able to export C<MODIFY_CODE_ATTRIBUTES> into the package, that uses attributes.
+
+This approach allow mix different attributes without patching C<UNIVERSAL>.
+
+  package Bar;
+
+  use Evo '-Loaded; -Attr attr_handler; -Export import';
+
+  attr_handler(
+    sub ($pkg, $code, @attrs) {
+      my @found     = grep {/^Bar/} @attrs;
+      my @remaining = grep { !/^Bar/ } @attrs;
+      say "found in $pkg ($code): " . join ', ', @found if @found;
+      @remaining;
+    }
+  );
+
+  # test2.pl
+  use Evo 'Foo *; Bar *';
+
+  sub multi : Foo : Bar(args) {...}
+
+=head2 attr_handler
+
+This method should be called only once. In fact, it adds C<MODIFY_CODE_ATTRIBUTES> to export list and call internal methods that register handler.
+
+1;
