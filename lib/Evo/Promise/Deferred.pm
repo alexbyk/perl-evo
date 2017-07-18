09b61f378d1d0e2714b4ca9f08e32c5ebc4dedc5
alexbyk <alex@alexbyk.com>
2017-07-19 01:53:00 +0300

    provide default undef argument for $d->resolve() and $d->reject()

--- lib/Evo/Promise/Deferred.pm
+++ lib/Evo/Promise/Deferred.pm
@@ -4,12 +4,12 @@ use Evo '-Class *';
 has 'promise', ro;
 has 'called', optional;
 
-sub reject ($self, $r) {
+sub reject ($self, $r=undef) {
   return if $self->called;
   $self->called(1)->promise->d_reject_continue($r);
 }
 
-sub resolve ($self, $v) {
+sub resolve ($self, $v=undef) {
   return if $self->called;
   $self->called(1)->promise->d_resolve_continue($v);
 }
